b45976e0276ff280b688a9dd4f849f5b29abfdbc
miguel-fg <miguelfierro55@gmail.com>
2023-09-26 16:59:20 -0700

    Added basic signup for user model

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,15 @@ const login_user = asyncHandler(async (req, res) => {
 
 //signup user
 const signup_user = asyncHandler(async (req, res) => {
-    res.json({ mssg: "signup user" });
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.signup(email, password);
+
+        res.status(200).json({email, user});
+    } catch(err){
+        res.status(400).json({error: err.message});
+    }
 });
 
 module.exports = { login_user, signup_user };
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -14,5 +15,23 @@ const userSchema = new Schema({
         required: true,
     },
 });
+
+// static signup method
+userSchema.statics.signup = async function(email, password) {
+    const exists = await this.findOne({ email }); //check for existing email
+
+    if(exists) {
+        throw Error("Email already in use");
+    }
+
+    //random string added to password
+    const salt = await bcrypt.genSalt(10);
+    
+    //hash and store the value to the db    
+    const hash = await bcrypt.hash(password, salt);
+    const user = await this.create({ email, password: hash });
+
+    return user;
+}
 
 module.exports = mongoose.model("User", userSchema);
