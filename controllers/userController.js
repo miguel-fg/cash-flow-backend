44e5db5bf6517ed4703a34dd6d33f942af41a91b
miguel-fg <miguelfierro55@gmail.com>
2023-09-27 11:48:47 -0700

    Added JWT to signup controller function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,11 @@
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+
+//function to generate Tokens for signup and login
+const createToken = (id) => {
+    return jwt.sign({_id: id}, process.env.SECRET, { expiresIn: '3d'});
+}
 
 //login user
 const login_user = asyncHandler(async (req, res) => {
@@ -13,7 +19,11 @@ const signup_user = asyncHandler(async (req, res) => {
     try {
         const user = await User.signup(email, password);
 
-        res.status(200).json({email, user});
+        //create token for the user
+        const token = createToken(user._id)
+
+        //sends the token back to the server
+        res.status(200).json({email, token});
     } catch(err){
         res.status(400).json({error: err.message});
     }
