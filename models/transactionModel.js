73ad536b1358dc6fec40c1075fddd26643bce16b
miguel-fg <miguelfierro55@gmail.com>
2023-09-28 12:27:25 -0700

    Added user_id property to transaction model

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 // transaction model
 const transactionSchema = new Schema(
     {
+        user_id: { type: String, required: true},
         title: { type: String, required: true },
         type: { type: String, required: true },
         amount: { type: Number, required: true },
